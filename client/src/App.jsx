bdc3373cee4b46e7dabb38a1f0401176982927a2
ajummer <makeudreamempty@gmail.com>
2023-12-26 23:40:49 +0200

    Add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx"
 import  "./app.css"
 function App() {
   return (
     <div className="link">
       <Router>
         <Navbar />
+        <Footer/>
         <Routes></Routes>
       </Router>
     </div>
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import styles from "./footer.module.css"
+
+export default function Footer() {
+  return (
+   <div className={styles.footer}>
+    <div className={styles.top}>
+        <div className={styles.item}>
+            <h1>Categories</h1>
+            <span>Women</span>
+            <span>Men</span>
+            <span>Children</span>
+            <span>Accessories</span>
+        </div>
+        <div className={styles.item}>
+            <h1>About</h1>
+            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore asperiores dolores, voluptas deserunt voluptatem optio dolor quod quibusdam non reiciendis.</span>
+        </div>
+        <div className={styles.item}>
+            <h1>Contact</h1>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ab sed aut expedita quisquam ad corporis blanditiis ullam sit illum?</span>
+        </div>
+    </div>
+    <div className={styles.bottom}>
+        <div className={styles.left}>
+            <div className={styles.logo}>Solid Style Hub</div>
+            <div className={styles.copyright}> © Copyright Ayturul Yumer 2024 . All rights reserved </div>
+        </div>
+        <div className={styles.right}>
+            <img src="paymentmethods.png" alt="paymentmethods" height={200}  width={450}/>
+        </div>
+    </div>
+   </div>
+  )
+}
