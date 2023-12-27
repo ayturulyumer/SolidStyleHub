859cf4558d784d49911c44b60e167d5a4ab2ab6f
ajummer <makeudreamempty@gmail.com>
2023-12-27 23:03:49 +0200

    Add dummy products

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -12,3 +12,32 @@ export const sliderImages = [
         title: "Shoes"
     },
 ];
+
+
+export const dummyProducts = [
+    {
+        id:1,
+        url:"https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title:"T-Shirt",
+        isNew: true,
+        oldPrice:20,
+        newPrice:15
+    },
+    {
+        id:2,
+        url:"https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title:"Hoodie",
+        isNew: true,
+        oldPrice:45,
+        newPrice:25
+    },
+    {
+        id:3,
+        url:"https://images.pexels.com/photos/2385477/pexels-photo-2385477.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        title:"Sneakers",
+        isNew: false,
+        oldPrice:35,
+        newPrice:20
+    },
+    
+]
