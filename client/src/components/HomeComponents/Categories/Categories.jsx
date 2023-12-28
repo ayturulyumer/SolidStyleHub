7617726e3f58b4a1cf0d20dffdcb10c4eef626ae
ajummer <makeudreamempty@gmail.com>
2023-12-28 14:21:55 +0200

    Add Categories in Home page

---
+++ client/src/components/HomeComponents/Categories/Categories.jsx
@@ -0,0 +1,76 @@
+import styles from "./categories.module.css";
+import { Link } from "react-router-dom";
+
+export default function Categories() {
+  return (
+    <div className={styles.categories}>
+      <div className={styles.col}>
+        <div className={styles.row}>
+          <img
+            src="https://images.pexels.com/photos/6347546/pexels-photo-6347546.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="sale"
+          />
+          <button>
+            <Link className="link" to="/products/sale">
+              Sale
+            </Link>
+          </button>
+        </div>
+        <div className={styles.row}>
+          <img
+            src="https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="women"
+          />
+          <button>
+            <Link className="link" to="/products/women">
+              Women
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className={styles.col}>
+        <div className={styles.row}>
+          <img
+            src="https://images.pexels.com/photos/2709563/pexels-photo-2709563.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="men"
+          />
+          <button>
+            <Link className="link" to="/products/men">
+              Men
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className={styles.colLarge}>
+        <div className={styles.row}>
+          <div className={styles.col}>
+            <div className={styles.row}>
+              <img
+                src="https://images.pexels.com/photos/10508388/pexels-photo-10508388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="children"
+              />
+              <button>
+                <Link className="link" to="/products/children">
+                  Children
+                </Link>
+              </button>
+            </div>
+          </div>
+          <div className={styles.col}>
+            <div className={styles.row}>
+              <img
+                src="https://images.pexels.com/photos/1503009/pexels-photo-1503009.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="shoes"
+              />
+              <button>
+                <Link className="link" to="/products/shoes">
+                  Shoes
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import styles from "./home.module.css"
 import Slider from "../../components/HomeComponents/Slider/Slider.jsx"
 import FeaturedProducts from "../../components/HomeComponents/FeaturedProducts/FeaturedProducts.jsx"
+import Categories from "../../components/HomeComponents/Categories/Categories.jsx"
 
 export default function Home() {
   return (
     <div className={styles.home}>
         <Slider/>
         <FeaturedProducts type="Featured"/>
+        <Categories/>
         <FeaturedProducts type="Trending"/>
     </div>
   )
