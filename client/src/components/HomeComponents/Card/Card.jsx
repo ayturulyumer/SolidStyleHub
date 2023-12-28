20c32a0b653dea59cfe26baead8bd17d3e4f6735
ajummer <makeudreamempty@gmail.com>
2023-12-28 12:24:29 +0200

    Add Featured Products Card

--- client/src/components/HomeComponents/Card/Card.jsx
+++ client/src/components/HomeComponents/Card/Card.jsx
@@ -2,9 +2,20 @@ import styles from "./card.module.css"
 import {Link} from "react-router-dom"
  
 export default function Card({product}) {
+  console.log(product.isNew)
   return (
-    <Link to={`/products/${product.id}`}>
-        <div className={styles.card}></div>
+    <Link className="link" to={`/products/${product.id}`}>
+        <div className={styles.card}>
+          <div className={styles.image}>
+            {product.isNew && <span>New Season</span>}
+            <img src={product.url} alt={product.title} className={styles.mainImg}/>
+          </div>
+          <h2>{product.title}</h2>
+          <div className={styles.prices}>
+            <h3>${product.oldPrice}</h3>
+            <h3>${product.newPrice}</h3>
+          </div>
+        </div>
     </Link>
   )
 }
--- client/src/components/HomeComponents/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/HomeComponents/FeaturedProducts/FeaturedProducts.jsx
@@ -1,6 +1,7 @@
 import styles from "./featuredProducts.module.css";
 import Card from "../Card/Card.jsx";
 import { dummyProducts } from "../../../constants/constants.js";
+console.log(dummyProducts)
 
 export default function FeaturedProducts({ type }) {
   return (
@@ -8,14 +9,12 @@ export default function FeaturedProducts({ type }) {
       <div className={styles.top}>
         <h1>{type} products</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem sunt debitis velit nisi reprehenderit veritatis iusto
-          optio facilis perferendis possimus?
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam aspernatur itaque corrupti corporis repellat adipisci harum nesciunt! Perferendis atque asperiores deserunt sit laudantium ullam ipsa, aspernatur fugit quod nihil beatae saepe ducimus eum minima! Corporis quia est iusto consequatur asperiores.
         </p>
       </div>
       <div className={styles.bottom}>
         {dummyProducts.map((product) => {
-          return <Card product={product} key={product.id} />;
+          return <Card product={product} key={product.id} />; 
         })}
       </div>
     </div>
