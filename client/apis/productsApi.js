a3ec4afc90bdbe93376fe143e4f0c0b43d8510c5
ajummer <makeudreamempty@gmail.com>
2023-12-31 13:48:01 +0200

    Populate the products with their images

--- client/apis/productsApi.js
+++ client/apis/productsApi.js
@@ -2,6 +2,6 @@ import * as request from "../lib/request.js";
 const baseUrl = import.meta.env.VITE_APP_URL;
 
 export const getAll = async () => {
-  const photos = await request.get(`${baseUrl}/products`);
+  const photos = await request.get(`${baseUrl}/products?populate=*`);
   return Object.values(photos.data)
 };
--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,13 +2,13 @@ import styles from "./card.module.css"
 import {Link} from "react-router-dom"
  
 export default function Card({product}) {
-  console.log(product)
+  const imageURL = import.meta.env.VITE_APP_IMAGE_URL
   return (
     <Link className="link" to={`/products/${product.id}`}>
         <div className={styles.card}>
           <div className={styles.image}>
             {product?.isNew && <span>New Season</span>}
-            <img src={product.url} alt={product.title} className={styles.mainImg}/>
+            <img src={imageURL + product.firstImg.data.attributes.url} alt={product.title} className={styles.mainImg}/>
           </div>
           <h2>{product?.title}</h2>
           <div className={styles.prices}>
