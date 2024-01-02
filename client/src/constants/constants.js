505472389aa2c2065e3bad151275e68e67f00022
ajummer <makeudreamempty@gmail.com>
2024-01-02 21:21:54 +0200

    Change namings

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -25,30 +25,30 @@ export const sliderImages = [
 export const dummyProducts = [
     {
         id:1,
-        url:"https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        firstImg:"https://images.pexels.com/photos/1656684/pexels-photo-1656684.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         title:"T-Shirt",
         isNew: true,
         oldPrice:20,
-        newPrice:15,
+        price:15,
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. In, explicabo odit? Possimus soluta cumque consequuntur, molestias, quo similique corporis dolorum hic voluptatum magnam vero aliquam iste facere veniam officia quasi! Cupiditate veniam sed asperiores nam neque maiores nulla aut. Quam nemo nostrum quo modi iure dolorem nesciunt impedit nihil totam autem non labore omnis provident fugit, facilis eveniet numquam maiores. Modi culpa voluptatibus temporibus necessitatibus ducimus natus, illo eaque minima corporis expedita explicabo quos ratione. Qui ratione culpa facere fugit similique aliquam labore accusantium, hic esse, maiores iste architecto quisquam facilis ullam quae quibusdam iusto tenetur itaque accusamus harum, aut eveniet asperiores a consectetur? A vero sapiente nemo reiciendis minus corrupti sint odio commodi, eum veniam, dolores nisi placeat praesentium voluptatum. Harum impedit mollitia labore culpa, beatae, ea temporibus reiciendis aliquid omnis quae illo saepe iure dolore ipsa dolorum aspernatur distinctio doloremque voluptatem? Nostrum sit saepe debitis voluptate eius exercitationem atque, culpa iusto ut dolorem corporis necessitatibus, earum, pariatur error perferendis doloremque distinctio? Dolore quam maiores ex doloremque. Quod, quidem porro? Soluta architecto assumenda veritatis culpa incidunt harum adipisci a exercitationem ipsa. Dignissimos, rem libero labore illum architecto doloribus veniam vel eveniet impedit maiores odio tenetur asperiores quis cupiditate quas."
     },
     {
         id:2,
-        url:"https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        firstImg:"https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         title:"Hoodie",
         isNew: true,
         oldPrice:45,
-        newPrice:25,
+        price:25,
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. In, explicabo odit? Possimus soluta cumque consequuntur, molestias, quo similique corporis dolorum hic voluptatum magnam vero aliquam iste facere veniam officia quasi! Cupiditate veniam sed asperiores nam neque maiores nulla aut. Quam nemo nostrum quo modi iure dolorem nesciunt impedit nihil totam autem non labore omnis provident fugit, facilis eveniet numquam maiores. Modi culpa voluptatibus temporibus necessitatibus ducimus natus, illo eaque minima corporis expedita explicabo quos ratione. Qui ratione culpa facere fugit similique aliquam labore accusantium, hic esse, maiores iste architecto quisquam facilis ullam quae quibusdam iusto tenetur itaque accusamus harum, aut eveniet asperiores a consectetur? A vero sapiente nemo reiciendis minus corrupti sint odio commodi, eum veniam, dolores nisi placeat praesentium voluptatum. Harum impedit mollitia labore culpa, beatae, ea temporibus reiciendis aliquid omnis quae illo saepe iure dolore ipsa dolorum aspernatur distinctio doloremque voluptatem? Nostrum sit saepe debitis voluptate eius exercitationem atque, culpa iusto ut dolorem corporis necessitatibus, earum, pariatur error perferendis doloremque distinctio? Dolore quam maiores ex doloremque. Quod, quidem porro? Soluta architecto assumenda veritatis culpa incidunt harum adipisci a exercitationem ipsa. Dignissimos, rem libero labore illum architecto doloribus veniam vel eveniet impedit maiores odio tenetur asperiores quis cupiditate quas."
 
     },
     {
         id:3,
-        url:"https://images.pexels.com/photos/2385477/pexels-photo-2385477.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        firstImg:"https://images.pexels.com/photos/2385477/pexels-photo-2385477.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         title:"Sneakers",
         isNew: false,
         oldPrice:35,
-        newPrice:20,
+        price:20,
         description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. In, explicabo odit? Possimus soluta cumque consequuntur, molestias, quo similique corporis dolorum hic voluptatum magnam vero aliquam iste facere veniam officia quasi! Cupiditate veniam sed asperiores nam neque maiores nulla aut. Quam nemo nostrum quo modi iure dolorem nesciunt impedit nihil totam autem non labore omnis provident fugit, facilis eveniet numquam maiores. Modi culpa voluptatibus temporibus necessitatibus ducimus natus, illo eaque minima corporis expedita explicabo quos ratione. Qui ratione culpa facere fugit similique aliquam labore accusantium, hic esse, maiores iste architecto quisquam facilis ullam quae quibusdam iusto tenetur itaque accusamus harum, aut eveniet asperiores a consectetur? A vero sapiente nemo reiciendis minus corrupti sint odio commodi, eum veniam, dolores nisi placeat praesentium voluptatum. Harum impedit mollitia labore culpa, beatae, ea temporibus reiciendis aliquid omnis quae illo saepe iure dolore ipsa dolorum aspernatur distinctio doloremque voluptatem? Nostrum sit saepe debitis voluptate eius exercitationem atque, culpa iusto ut dolorem corporis necessitatibus, earum, pariatur error perferendis doloremque distinctio? Dolore quam maiores ex doloremque. Quod, quidem porro? Soluta architecto assumenda veritatis culpa incidunt harum adipisci a exercitationem ipsa. Dignissimos, rem libero labore illum architecto doloribus veniam vel eveniet impedit maiores odio tenetur asperiores quis cupiditate quas."
 
     },
