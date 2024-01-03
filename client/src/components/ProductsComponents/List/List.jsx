46177eba75cfb345e17d5b6015aab776f465ca42
ajummer <makeudreamempty@gmail.com>
2024-01-03 10:38:20 +0200

    Fetch by filtered price

--- client/src/components/ProductsComponents/List/List.jsx
+++ client/src/components/ProductsComponents/List/List.jsx
@@ -1,14 +1,17 @@
 import styles from "./list.module.css";
 import Card from "../../Card/Card.jsx";
-import {useFetch} from "../../../hooks/useFetch.js"
+import { useFetch } from "../../../hooks/useFetch.js";
 
 export default function List({ categoryId, maxPrice, sort, subCats }) {
-
-  {/** Initially fetch all the products related to the current category */}
-  const {data, loading, error} = useFetch(
-    `/products?populate=*&[filters][categories][id][$eq]=${categoryId}${subCats.map(category => `&[filters][sub_categories][id][$eq]=${category}`)}`
+  {
+    /** Initially fetch all the products related to the current category */
+  }
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][categories][id][$eq]=${categoryId}${subCats.map(
+      (category) => `&[filters][sub_categories][id][$eq]=${category}`
+    )}&[filters][price][$lte]=${maxPrice}`
   );
- 
+
   return (
     <div className={styles.list}>
       {data?.map((product) => (
