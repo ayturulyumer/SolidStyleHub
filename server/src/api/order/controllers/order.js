9daa60d61b5639d9746215afbcf16dc800e903d0
ajummer <makeudreamempty@gmail.com>
2024-01-06 11:28:26 +0200

    write the backend logic of stripe
    idk what's going on actually ..

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -1,9 +1,52 @@
-'use strict';
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+("use strict");
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { products, stripeId } = ctx.request.body;
+    const lineItems = await Promise.all(
+      products.map(async (item) => {
+        const item = await strapi
+          .service("api:product.product")
+          .findOne(item.id);
+
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: item.title,
+            },
+            unit_amout: item.price * 100,
+          },
+          quantity: item.quantity,
+        };
+      })
+    );
+    try {
+      const session = await stripe.checkout.sessions.create({
+        mode: "payment",
+        success_url: `${process.env.CLIENT_URL}?success=true`,
+        cancel_url: `${process.env.CLIENT_URL}?canceled=false`,
+        line_items: lineItems,
+        shipping_address_collection: { allowed_countries: null },
+        payment_method_types: ["card"],
+      });
+      await strapi.service("api::order:order").create({
+        data: {
+          products,
+          stripeId: session.id,
+        },
+      });
+      return { stripeSession: session };
+    } catch (err) {
+      ctx.response.status = 500;
+      return err;
+    }
+  },
+}));
